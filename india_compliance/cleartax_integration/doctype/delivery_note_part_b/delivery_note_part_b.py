7354f6ddc7ea70525a01f609ef10317c6a46cda1
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2022-11-09 12:30:16 +0530

    feat: update part B

---
+++ india_compliance/cleartax_integration/doctype/delivery_note_part_b/delivery_note_part_b.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2022, 8848 Digital LLP and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class DeliveryNotePartB(Document):
+	def on_submit(self):
+		try:
+			dn = frappe.get_doc('Delivery Note',self.delivery_note)
+			dn.lr_no = self.lr_no
+			dn.lr_date = self.lr_date
+			dn.vehicle_no = self.vehicle_no
+			dn.distance = self.distance
+			dn.mode_of_transport = self.mode_of_transport
+		except Exception as e:
+			frappe.logger('cleartax').exception(e)
