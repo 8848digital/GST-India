298e40172efadd413e8d80707cf85cd93784f9aa
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2022-11-11 13:38:53 +0530

    feat: import fix

--- india_compliance/cleartax_integration/doctype/cleartax_settings/cleartax_settings.py
+++ india_compliance/cleartax_integration/doctype/cleartax_settings/cleartax_settings.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2022, Resilient Tech and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class CleartaxSettings(Document):
