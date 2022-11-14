88203a1b8c24269857f14348472c93becb2add96
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2022-11-14 14:58:40 +0530

    fix: part b update

--- india_compliance/cleartax_integration/doctype/delivery_note_part_b/delivery_note_part_b.py
+++ india_compliance/cleartax_integration/doctype/delivery_note_part_b/delivery_note_part_b.py
@@ -7,11 +7,10 @@ from frappe.model.document import Document
 class DeliveryNotePartB(Document):
 	def on_submit(self):
 		try:
-			dn = frappe.get_doc('Delivery Note',self.delivery_note)
-			dn.lr_no = self.lr_no
-			dn.lr_date = self.lr_date
-			dn.vehicle_no = self.vehicle_no
-			dn.distance = self.distance
-			dn.mode_of_transport = self.mode_of_transport
+			frappe.db.set_value('Delivery Note',self.delivery_note,'lr_no',self.lr_no)
+			frappe.db.set_value('Delivery Note',self.delivery_note,'lr_date',self.lr_date)
+			frappe.db.set_value('Delivery Note',self.delivery_note,'vehicle_no',self.vehicle_no)
+			frappe.db.set_value('Delivery Note',self.delivery_note,'distance',self.distance)
+			frappe.db.set_value('Delivery Note',self.delivery_note,'mode_of_transport',self.mode_of_transport)
 		except Exception as e:
 			frappe.logger('cleartax').exception(e)
