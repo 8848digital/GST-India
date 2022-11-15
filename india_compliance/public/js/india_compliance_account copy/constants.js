0f6fefd4671479111ed93b2442db1e0c785fa636
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2022-11-15 11:15:18 +0530

    fix copy remove

--- india_compliance/public/js/india_compliance_account copy/constants.js
+++
@@ -1,6 +0,0 @@
-export const UiState = Object.freeze({
-    initial: 0,
-    loading: 1,
-    success: 2,
-    error: 3,
-});
--- india_compliance/public/js/india_compliance_account copy/utils.js
+++
@@ -1,3 +0,0 @@
-export const getReadableNumber = function (num, precision = 2) {
-    return format_number(num, null, precision);
-};
