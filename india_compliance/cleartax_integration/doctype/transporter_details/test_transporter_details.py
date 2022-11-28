ea68860a1f1e1919604b44630e7ca88f2c1c19c5
Sankara Subramanian <sankarsubramaniankvs@gmail.com>
2022-11-28 11:22:12 +0530

    feat: Transporter Details

---
+++ india_compliance/cleartax_integration/doctype/transporter_details/test_transporter_details.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, 8848 Digital LLP and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestTransporterDetails(FrappeTestCase):
+	pass
---
+++ india_compliance/cleartax_integration/doctype/transporter_details/transporter_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, 8848 Digital LLP and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Transporter Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
