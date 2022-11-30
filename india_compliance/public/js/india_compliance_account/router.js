719dc464a1cf1f6cd14f4e03aa1f34ddd0ac9001
milanpethani <108564035+milanpethani@users.noreply.github.com>
2022-11-30 17:33:58 +0530

    JS Fixes

--- india_compliance/public/js/india_compliance_account/router.js
+++ india_compliance/public/js/india_compliance_account/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import VueRouter from "vue-router";
 
 import AuthPage from "./pages/AuthPage.vue";
 import AccountPage from "./pages/AccountPage.vue";
@@ -35,8 +35,9 @@ const routes = [
     }
 ];
 
-export const router = createRouter({
-    history: createWebHistory("/app/india-compliance-account"),
+export default new VueRouter({
+    mode: "history",
+    base: "/app/india-compliance-account",
     routes: routes,
 });
 
