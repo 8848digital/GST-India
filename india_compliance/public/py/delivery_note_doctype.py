efa8704e06c0d7402dc88f1ddf0d3e04dbfb117b
Sankara Subramanian V <sankarsubramaniankvs@gmail.com>
2023-02-04 16:29:10 +0530

    Update delivery_note_doctype.py

--- india_compliance/public/py/delivery_note_doctype.py
+++ india_compliance/public/py/delivery_note_doctype.py
@@ -2,7 +2,7 @@ from india_compliance.cleartax_integration.API.ewb import ewb_without_irn
 import frappe 
 
 def delivery_note_submit(doc, method=None):
-    if frappe.get_value('Cleartax Settings', 'Cleartax Settings','automate'):
+    if frappe.db.get_single_value('Cleartax Settings','automate'):
         ewb_without_irn(**{'delivery_note':doc.name})
 
 def delivery_note_save(doc,method=None):
