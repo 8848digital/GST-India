8027154b9600e659c0f7a106abc2e036444e704c
Shyam Rathod <shrathod@dexciss.com>
2024-02-20 15:52:31 +0530

    create doctype

---
+++ gst_india/gst_india/doctype/generate_group_id/generate_group_id.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, 8848 Digital LLP and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Generate Group ID', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ gst_india/gst_india/doctype/generate_group_id/test_generate_group_id.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, 8848 Digital LLP and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGenerateGroupID(FrappeTestCase):
+	pass
